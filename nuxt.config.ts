efa28481cb2490cc362924a83e9fe5c91efdb533
yaasir cheekoory <yaasir1997@gmail.com>
2025-01-17 22:57:38 +0400

    with nuxt ui now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,19 @@ import { resolve } from "path"
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   alias: {
     "@": resolve(__dirname, "/")
   },
+
   css: ["~/assets/scss/main.scss"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxt/ui"],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { CircleArrowRight, CircleArrowLeft } from 'lucide-vue-next';
+import BackGroundImage from '../assets/image/bg.png'
 </script>
 
 <template>
-  <div class="h-dvh bg-slate-800 flex flex-col gap-2 justify-center items-center font-serif">
-    <p class="text-[white] text-5xl flex justify-center items-center gap-3">
+  <div class="h-dvh flex flex-col gap-2 justify-center items-center font-serif relative">
+    <img :src="BackGroundImage" class="absolute w-full h-dvh"  />
+    <p class="text-[white] text-5xl flex justify-center items-center gap-3 z-10">
       <CircleArrowRight />
       Nuxt 3 Starter Pack
       <CircleArrowLeft />
     </p>
-    <p class="text-[purple] text-3xl">With Alias, SCSS, Tailwind Css and Lucide for icons</p>
+    <p class="text-[white] text-3xl z-10">With Alias, SCSS, Tailwind, Lucide and Nuxt UI</p>
   </div>
 </template>
